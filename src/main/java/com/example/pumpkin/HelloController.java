2ac1894a5dd8d882d34b32a8effcfda479b5e279
mgr87 <maximillian.rubensson@iths.se>
2024-10-16 14:43:08 +0200

    generate setters and getters

--- src/main/java/com/example/pumpkin/HelloController.java
+++ src/main/java/com/example/pumpkin/HelloController.java
@@ -1,12 +1,6 @@
 package com.example.pumpkin;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
-
 import javax.swing.text.html.ImageView;
 
 public class HelloController {
--- src/main/java/com/example/pumpkin/Model.java
+++ src/main/java/com/example/pumpkin/Model.java
@@ -4,13 +4,48 @@ import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.scene.image.Image;
 
-import java.awt.*;
 
 public class Model {
 
     private int score = 0;
     Image noSmash;
     Image smash;
+
+    public Image getFirstPumpkin() {
+        return firstPumpkin.get();
+    }
+
+    public ObjectProperty<Image> firstPumpkinProperty() {
+        return firstPumpkin;
+    }
+
+    public void setFirstPumpkin(Image firstPumpkin) {
+        this.firstPumpkin.set(firstPumpkin);
+    }
+
+    public Image getSecondPumpkin() {
+        return secondPumpkin.get();
+    }
+
+    public ObjectProperty<Image> secondPumpkinProperty() {
+        return secondPumpkin;
+    }
+
+    public void setSecondPumpkin(Image secondPumpkin) {
+        this.secondPumpkin.set(secondPumpkin);
+    }
+
+    public Image getThirdPumpkin() {
+        return thirdPumpkin.get();
+    }
+
+    public ObjectProperty<Image> thirdPumpkinProperty() {
+        return thirdPumpkin;
+    }
+
+    public void setThirdPumpkin(Image thirdPumpkin) {
+        this.thirdPumpkin.set(thirdPumpkin);
+    }
 
     ObjectProperty<Image> firstPumpkin;
     ObjectProperty<Image> secondPumpkin;
@@ -20,7 +55,7 @@ public class Model {
         noSmash = new Image(getClass().getResource("images/pumpkin_blank.png").toExternalForm());
         smash = new Image(getClass().getResource("images/pumpkin1.png").toExternalForm());
 
-        firstPumpkin = new SimpleObjectProperty<>(noSmash);
+        firstPumpkin = new SimpleObjectProperty<>(smash);
         secondPumpkin = new SimpleObjectProperty<>(noSmash);
         thirdPumpkin = new SimpleObjectProperty<>(noSmash);
 
@@ -30,6 +65,7 @@ public class Model {
     public void pumpkinSmashed(Pumpkin pumpkin) {
         //Check if we can smack this pumpkin
         //Increase score
+
 
         score += 10;
         System.out.println(getScore());
