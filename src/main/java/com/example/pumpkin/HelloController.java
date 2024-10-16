15b677d12ef74bfe4007528607b01718f281a34e
mgr87 <maximillian.rubensson@iths.se>
2024-10-16 13:06:37 +0200

    Latest

--- src/main/java/com/example/pumpkin/HelloController.java
+++ src/main/java/com/example/pumpkin/HelloController.java
@@ -2,13 +2,42 @@ package com.example.pumpkin;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+
+    HelloController() {
+        textLabel.textProperty().bind(model.userTextProperty());
+    }
+
+    public void Initialize() {
+        textLabel.textProperty().bind(model.userTextProperty());
+        textFieldTarget.textProperty().bind(model.userTextProperty());
+    }
+
+    public Model model = new Model();
+
+    public Button transferButton;
+    public TextField textField;
+    public Label textLabel;
+    public TextField textFieldTarget;
+
+    public Model getModel() {
+        return model;
+    }
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void transferButtonAction() {
+        model.setUserText(textField.getText());
+
+        textFieldTarget.setText(model.getUserText());
+        textLabel.setText(model.getUserText());
+    }
+
+    public void textFieldChanged(KeyEvent inputMethodEvent) {
+        transferButton.setDisable(textField.getText().isEmpty());
+
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.pumpkin {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
 
     opens com.example.pumpkin to javafx.fxml;
