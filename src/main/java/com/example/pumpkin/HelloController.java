9100948e42f46b3c258f162a0b645bff40e75d9f
mgr87 <maximillian.rubensson@iths.se>
2024-10-16 13:10:44 +0200

    new project

--- src/main/java/com/example/pumpkin/HelloController.java
+++ src/main/java/com/example/pumpkin/HelloController.java
@@ -8,36 +8,4 @@ import javafx.scene.input.KeyEvent;
 
 public class HelloController {
 
-    HelloController() {
-        textLabel.textProperty().bind(model.userTextProperty());
-    }
-
-    public void Initialize() {
-        textLabel.textProperty().bind(model.userTextProperty());
-        textFieldTarget.textProperty().bind(model.userTextProperty());
-    }
-
-    public Model model = new Model();
-
-    public Button transferButton;
-    public TextField textField;
-    public Label textLabel;
-    public TextField textFieldTarget;
-
-    public Model getModel() {
-        return model;
-    }
-
-    @FXML
-    protected void transferButtonAction() {
-        model.setUserText(textField.getText());
-
-        textFieldTarget.setText(model.getUserText());
-        textLabel.setText(model.getUserText());
-    }
-
-    public void textFieldChanged(KeyEvent inputMethodEvent) {
-        transferButton.setDisable(textField.getText().isEmpty());
-
-    }
 }
--- src/main/java/com/example/pumpkin/Model.java
+++ src/main/java/com/example/pumpkin/Model.java
@@ -5,17 +5,5 @@ import javafx.beans.property.StringProperty;
 import javafx.beans.value.ObservableValue;
 
 public class Model {
-    private StringProperty userText = new SimpleStringProperty();
 
-    public void setUserText(String userText){
-        this.userText.set(userText);
-    }
-
-    public String getUserText(){
-        return userText.get();
-    }
-
-    public ObservableValue<String> userTextProperty() {
-        return userText;
-    }
 }
