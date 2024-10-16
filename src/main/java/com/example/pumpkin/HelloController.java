b822f061e6442f098ade8554a39e6749cd80b468
mgr87 <maximillian.rubensson@iths.se>
2024-10-16 14:35:00 +0200

    add calls to model

--- src/main/java/com/example/pumpkin/HelloController.java
+++ src/main/java/com/example/pumpkin/HelloController.java
@@ -5,8 +5,13 @@ import javafx.scene.control.Label;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+
+import javax.swing.text.html.ImageView;
 
 public class HelloController {
+
+    public ImageView image1;
 
     public void innitialize() {
 
@@ -14,4 +19,17 @@ public class HelloController {
 
     private Model model = new Model();
 
+
+    public void image1Clicked(MouseEvent mouseEvent) {
+        model.pumpkinSmashed(Pumpkin.FIRST);
+    }
+
+    public void image2Clicked(MouseEvent mouseEvent) {
+        model.pumpkinSmashed(Pumpkin.SECOND);
+    }
+
+    public void image3Clicked(MouseEvent mouseEvent) {
+        model.pumpkinSmashed(Pumpkin.THIRD);
+
+    }
 }
--- src/main/java/com/example/pumpkin/Model.java
+++ src/main/java/com/example/pumpkin/Model.java
@@ -1,9 +1,47 @@
 package com.example.pumpkin;
 
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
-import javafx.beans.value.ObservableValue;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.scene.image.Image;
+
+import java.awt.*;
 
 public class Model {
 
+    private int score = 0;
+    Image noSmash;
+    Image smash;
+
+    ObjectProperty<Image> firstPumpkin;
+    ObjectProperty<Image> secondPumpkin;
+    ObjectProperty<Image> thirdPumpkin;
+
+    public Model() {
+        noSmash = new Image(getClass().getResource("images/pumpkin_blank.png").toExternalForm());
+        smash = new Image(getClass().getResource("images/pumpkin1.png").toExternalForm());
+
+        firstPumpkin = new SimpleObjectProperty<>(noSmash);
+        secondPumpkin = new SimpleObjectProperty<>(noSmash);
+        thirdPumpkin = new SimpleObjectProperty<>(noSmash);
+
+
+    }
+
+    public void pumpkinSmashed(Pumpkin pumpkin) {
+        //Check if we can smack this pumpkin
+        //Increase score
+
+        score += 10;
+        System.out.println(getScore());
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
 }
+
+
